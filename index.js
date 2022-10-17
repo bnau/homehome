e4898882829d5095c8b20f875fa19e02e9d338f1
naube <bertrand.nau@talan.com>
2022-10-17 14:04:07 +0200

    feat: trigger actions with entities

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-import {speech} from './tts.js';
 import {getTrackUrls} from "./media-source.js";
 import {play} from "./player.js";
+import {
+    RasaNodeActionServer,
+    RasaActionEvent,
+} from "rasa-node-action-server";
 
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-play(await getTrackUrls("Chateaubriand"));
+const rnas = new RasaNodeActionServer();
 
-speech('salut! Ça va?')
+rnas.define("play_audio_book_action", async (action, res) => {
+    const author = action.tracker.latest_message.entities.filter(e=>e.entity==="author")[0].value;
+
+    play(await getTrackUrls(author));
+
+    res.addEvent(
+            RasaActionEvent.bot(
+                `D'accord, je lance un livre de ${author}`
+            )
+        )
+        .send();
+});
+
+rnas.start();
+
--- main.py
+++ main.py
@@ -2,19 +2,16 @@ import asyncio
 from rasa.model import get_latest_model
 from rasa.core.agent import Agent
 from tts.main import say
+from rasa.utils.endpoints import EndpointConfig
 
 
 def load_interpreter(model_path):
     nlu_model = get_latest_model(model_path)
-    return Agent.load(nlu_model)
+    return Agent.load(model_path=nlu_model, action_endpoint=EndpointConfig(url="http://localhost:5055"))
 
 
 agent = load_interpreter("chatbot/models")
 
-result = asyncio.run(agent.handle_text("Hello"))
-
-say(result[0]['text'])
-
-result = asyncio.run(agent.handle_text("Non, ça va pas trop"))
+result = asyncio.run(agent.handle_text("lis un livre de Chateaubriand"))
 
 say(result[0]['text'])
